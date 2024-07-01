5b8525d601b1a1085882534caa704483c25cab34
Mishan20 <ishansenanayaka1@gmail.com>
2024-07-01 11:59:42 +0530

    First commit

---
+++ src/test/java/com/learn/RegisterLogin/RegisterLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.RegisterLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
